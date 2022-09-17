52bd706e820e6db21ec75bdd442a962dae9d1f9d
Thenlie <leithen113@gmail.com>
2022-09-16 22:10:13 -0600

    Create screen directory. Move HomeScreen component into directory.

--- App.tsx
+++ App.tsx
@@ -1,35 +1,9 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
-
-import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import React from 'react';
+import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const HomeScreen = () => {
-    return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-    </View>
-    );  
-}
+import HomeScreen from './screens/Home';
 
 const Stack = createNativeStackNavigator();
 
