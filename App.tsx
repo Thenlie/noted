092dbeddc444dc6853ae666a9020bf4beed1c773
Thenlie <leithen113@gmail.com>
2022-09-21 19:35:20 -0600

    Update navigator style

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from './RootStackParams';
@@ -13,7 +12,17 @@ const App = () => {
 
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home">
+        <Stack.Navigator 
+            initialRouteName="Home"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#444444',
+                },
+                headerTintColor: '#ffffff',
+                headerLargeTitle: true,
+                headerTransparent: true,
+            }}    
+        >
             <Stack.Screen 
                 name="Home" 
                 component={HomeScreen}
